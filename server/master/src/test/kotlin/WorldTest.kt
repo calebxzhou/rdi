82f3694431196bd7c27ca42a92b06bd4cbd824fd
calebxzhou <me@calebxzhou.cn>
2026-03-01 23:57:39 +0800

    misc

--- server/master/src/test/kotlin/WorldTest.kt
+++ server/master/src/test/kotlin/WorldTest.kt
@@ -1,11 +1,18 @@
 import calebxzhou.rdi.common.anvilrw.format.AnvilReader
+import net.benwoodworth.knbt.nbtCompound
+import net.benwoodworth.knbt.nbtInt
+import net.benwoodworth.knbt.nbtList
 import java.io.File
 import kotlin.test.Test
 
 class WorldTest {
     @Test
     fun load(){
-        AnvilReader(File("C:\\Users\\calebxzhou\\Documents\\coding\\rdi5\\client\\ui\\run\\mc\\versions\\695519fec0e579e94b5c40c5_1.1.5.4\\saves\\新的世界\\region\\r.0.0.mca"))
-            .readRegion().chunks.filter { !it.isEmpty }.forEach { it.getNbtData()?.values?.map { println(it.toString()) } }
+        val chunkData =
+            AnvilReader(File("C:\\Users\\calebxzhou\\Documents\\RDI5sea-Ref\\.minecraft\\versions\\FTB Skies 2\\saves\\新的世界\\region\\r.0.0.mca"))
+                .readRegion().chunks.filter { !it.isEmpty }.first().getNbtData()
+        chunkData?.get("blending_data")?.nbtCompound?.get("min_section")?.nbtInt?.let { println(it) }
+        chunkData?.get("sections")?.nbtList?.forEach { println(it.toString())
+            println("---")}
     }
 }
