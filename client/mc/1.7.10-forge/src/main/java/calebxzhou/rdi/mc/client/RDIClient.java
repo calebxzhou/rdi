9f8d6185d38990862226c55fd8459de51b2fa28b
calebxzhou <me@calebxzhou.cn>
2026-04-07 23:26:53 +0800

    prepare 1.7 compatible

---
+++ client/mc/1.7.10-forge/src/main/java/calebxzhou/rdi/mc/client/RDIClient.java
@@ -0,0 +1,47 @@
+package calebxzhou.rdi.mc.client;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+
+@Mod(modid = "rdi", version = "1", name = "rdi", acceptedMinecraftVersions = "[1.7.10]")
+public class RDIClient {
+
+    public static final Logger LOG = LogManager.getLogger("rdi");
+
+    @SidedProxy(
+        clientSide = "calebxzhou.rdi.mc.client.ClientProxy",
+        serverSide = "calebxzhou.rdi.mc.client.CommonProxy")
+    public static CommonProxy proxy;
+
+    @Mod.EventHandler
+    // preInit "Run before anything else. Read your config, create blocks, items, etc, and register them with the
+    // GameRegistry." (Remove if not needed)
+    public void preInit(FMLPreInitializationEvent event) {
+        proxy.preInit(event);
+    }
+
+    @Mod.EventHandler
+    // load "Do your mod setup. Build whatever data structures you care about. Register recipes." (Remove if not needed)
+    public void init(FMLInitializationEvent event) {
+        proxy.init(event);
+    }
+
+    @Mod.EventHandler
+    // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(event);
+    }
+
+    @Mod.EventHandler
+    // register server commands in this event handler (Remove if not needed)
+    public void serverStarting(FMLServerStartingEvent event) {
+        proxy.serverStarting(event);
+    }
+}
